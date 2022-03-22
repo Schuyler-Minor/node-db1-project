4725ad0613b296fc5d14a483aa02e1110e97e8e6
Schuyler <skym1n0r91@gmail.com>
2022-03-21 19:01:25 -0500

    delete works

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -48,10 +48,10 @@ router.put(
   }
 );
 
-router.delete("/:id", md.checkAccountId, (req, res, next) => {
-  // DO YOUR MAGIC
+router.delete("/:id", md.checkAccountId, async (req, res, next) => {
   try {
-    res.json("get accounts");
+    await Account.deleteById(req.params.id);
+    res.json(req.account);
   } catch (err) {
     next(err);
   }
